356b1dc13f539908637356149cfa0c01e1b1ee5b
Yuta <yuta5196@gmail.com>
2020-05-27 14:10:06 +0900

    Initial Commit

---
+++ Swift5WKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKWebView
+//
+//  Created by yuta on 2020/05/27.
+//  Copyright © 2020 yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
